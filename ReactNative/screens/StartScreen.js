1d36ff72b24f612700e2736a97ba3ba922e95f14
zd247 <zduy2407@gmail.com>
2020-05-04 18:33:50 +0700

    fixed flex

--- ReactNative/screens/StartScreen.js
+++ ReactNative/screens/StartScreen.js
@@ -87,11 +87,10 @@ const styles = EStyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignSelf: 'center',
-		paddingVertical: '120rem',
 	},
 	footer: {
 		justifyContent: 'center',
-		flex: 3,
+		flex: 1,
 		backgroundColor: 'white',
 		borderTopLeftRadius: '30rem',
 		borderTopRightRadius: '30rem',
